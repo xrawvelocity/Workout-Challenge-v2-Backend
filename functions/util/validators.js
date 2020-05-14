48de94d9ba12f53997b7678ae4d80f8f23f86a81
Victor Fernandez <vmfs99@gmail.com>
2020-05-14 00:34:45 -0400

    sign up extra validation

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -5,6 +5,20 @@ const isEmail = (email) => {
   } else return false;
 };
 
+const isPassword = (password) => {
+  const regEx = /^[\w@-]{6,20}$/;
+  if (password.match(regEx)) {
+    return true;
+  } else return false;
+};
+
+const isUsername = (username) => {
+  const regEx = /^[a-z\d]{5,16}$/;
+  if (username.match(regEx)) {
+    return true;
+  } else return false;
+};
+
 const isEmpty = (string) => {
   if (string.trim() === "") {
     return true;
@@ -19,11 +33,18 @@ exports.validateSignupData = (data) => {
   } else if (!isEmail(data.email)) {
     errors.email = "Must be a valid email address";
   }
-  if (isEmpty(data.password)) errors.password = "Must not be empty";
+  if (isEmpty(data.password)) {
+    errors.password = "Must not be empty";
+  } else if (!isPassword(data.password)) {
+    error.password = "Must be alphanumeric and be 6–20 characters"
+  }
   if (data.password !== data.confirmPassword)
     errors.confirmPassword = "Passwords must match";
-  if (isEmpty(data.handle)) errors.handle = "Must not be empty";
-
+  if (isEmpty(data.handle)) {
+    errors.handle = "Must not be empty";
+  } else if(!isUsername(data.handle)) {
+    error.handle = "Must be alphanumeric and contain 5–16 characters"
+  }
   return {
     errors,
     valid: Object.keys(errors).length === 0 ? true : false,
@@ -43,15 +64,15 @@ exports.validateLoginData = (data) => {
 };
 
 exports.reduceUserDetails = (data) => {
-  let userDetails = {}
+  let userDetails = {};
 
-  if(!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
-  if(!isEmpty(data.website.trim())){
-    if(data.website.trim().substring(0,4) !== 'http'){
+  if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+  if (!isEmpty(data.website.trim())) {
+    if (data.website.trim().substring(0, 4) !== "http") {
       userDetails.website = `http://${data.website.trim()}`;
-    } else userDetails.website = data.website
+    } else userDetails.website = data.website;
   }
-  if(!isEmpty(data.location.trim())) userDetails.location = data.location;
+  if (!isEmpty(data.location.trim())) userDetails.location = data.location;
 
   return userDetails;
-}
+};
